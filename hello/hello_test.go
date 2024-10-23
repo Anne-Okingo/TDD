2d6600072348f74af33c53c13318ebd6c1c24e50
Your Name <you@example.com>
2024-10-23 13:03:34 +0300

    test: add unit test for hello

--- hello/hello_test.go
+++ hello/hello_test.go
@@ -7,19 +7,28 @@ import (
 func TestHello(t*testing.T){
 	t.Run("saying hello to people", func(t*testing.T){
 		input := "Alice"
-		got := Hello(input)
+		got := Hello(input, "")
 		want := "Hello, Alice"
 	
-		if got != want{
-			t.Errorf("Test Hello Failed got %q; want %q\n", got, want)
-		}
+
+		assertMessage(t,got, want)
 	})
 	t.Run("say 'Hello, World' when an empty string is supplied", func(t*testing.T){
-		got := Hello("")
+		got := Hello("","")
 		want := "Hello, World"
 	
-		if got != want{
-			t.Errorf("Test Hello Failed got %q; want %q\n", got, want)
-		}
+		assertMessage(t,got,want)
 	})
+	t.Run("in German", func(t*testing.T){
+		got := Hello("Temo", "Deutsch")
+		want := "Hallo, Temo"
+		assertMessage(t,got,want)
+	})
+}
+
+func assertMessage(t testing.TB, got, want string){
+	t.Helper()
+	if got != want{
+		t.Errorf("Testing Hello failed got %q; want %q\n",got, want)
+	}
 }
