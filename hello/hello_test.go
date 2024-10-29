6d7527cf69cb311e8e8c41c9e6a3ad00a063bcd8
Your Name <you@example.com>
2024-10-29 09:53:59 +0300

    test: improve unit test for hello world

--- hello/hello_test.go
+++ hello/hello_test.go
@@ -4,31 +4,30 @@ import (
 	"testing"
 )
 
-func TestHello(t*testing.T){
-	t.Run("saying hello to people", func(t*testing.T){
+func TestHello(t *testing.T) {
+	t.Run("say Hello with name only", func(t *testing.T) {
 		input := "Alice"
 		got := Hello(input, "")
 		want := "Hello, Alice"
-	
+		assertMessage(t, got, want)
+	})
 
-		assertMessage(t,got, want)
-	})
-	t.Run("say 'Hello, World' when an empty string is supplied", func(t*testing.T){
-		got := Hello("","")
+	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
+		got := Hello("", "")
 		want := "Hello, World"
-	
-		assertMessage(t,got,want)
+		assertMessage(t, got, want)
 	})
-	t.Run("in German", func(t*testing.T){
+
+	t.Run("in German", func(t *testing.T) {
 		got := Hello("Temo", "Deutsch")
 		want := "Hallo, Temo"
-		assertMessage(t,got,want)
+		assertMessage(t, got, want)
 	})
 }
 
-func assertMessage(t testing.TB, got, want string){
+func assertMessage(t testing.TB, got, want string) {
 	t.Helper()
-	if got != want{
-		t.Errorf("Testing Hello failed got %q; want %q\n",got, want)
+	if got != want {
+		t.Errorf("Testing Hello failed; got %q; want %q\n", got, want)
 	}
 }
